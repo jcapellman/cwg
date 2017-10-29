29eb049c0cad03d0b86a0ba2ef46a6d54a05322c
Jarred Capellman <jcapellman@hotmail.com>
2017-10-28 19:22:28 -0500

    Added: Initial Commit

---
+++ src/cwg.web/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Security.Cryptography;
+
+using Microsoft.AspNetCore.Mvc;
+
+namespace cwg.web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+        
+        private int getRandomInt(int min = 1, int max = 100) => new Random((int)DateTime.Now.Ticks).Next(min, max);
+
+        private static void FillArray(byte[] bytes)
+        {
+            new Random((int)DateTime.Now.Ticks).NextBytes(bytes);
+        }
+
+        public IActionResult Generate()
+        {
+            var originalBytes = System.IO.File.ReadAllBytes("sourcePE");
+
+            var newBytes = new byte[getRandomInt()];
+            
+            FillArray(newBytes);
+
+            for (var y = 0; y < newBytes.Length; y++)
+            {
+                originalBytes[originalBytes.Length - 1 - y] = newBytes[y];
+            }
+
+            string sha1Sum;
+
+            using (var shaManager = new SHA1Managed())
+            {
+                sha1Sum = BitConverter.ToString(shaManager.ComputeHash(originalBytes)).Replace("-", "");
+            }
+
+            return new FileContentResult(originalBytes, "application/octet-stream")
+            {
+                FileDownloadName = $"{sha1Sum}.exe"
+            };
+        }
+    }
+}
---
+++ src/cwg.web/Startup.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace cwg.web
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseDeveloperExceptionPage();
+            
+            app.UseStaticFiles();
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
