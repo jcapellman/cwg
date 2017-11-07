1fc017fade4920b0446b420828f6b608b0d5eaaf
Jarred Capellman <jcapellman@hotmail.com>
2017-11-06 19:57:09 -0600

    Added: PE32+ Generator

---
+++ src/cwg.web/Generators/PE32PlusGenerator.cs
@@ -0,0 +1,9 @@
+﻿namespace cwg.web.Generators
+{
+    public class PE32PlusGenerator : BaseGenerator
+    {
+        public override string GetName() => "PE32+";
+
+        public override string GetSourceName() => "sourcePE+.exe";
+    }
+}
