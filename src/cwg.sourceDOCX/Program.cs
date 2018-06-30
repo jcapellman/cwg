a56bcca2ba801c6af524bd61731a1a6f25de135d
Jarred Capellman <jcapellman@hotmail.com>
2018-06-29 21:44:43 -0500

    Added: source files and DOCX Source

---
+++ src/cwg.sourceDOCX/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.IO;
+
+using DocumentFormat.OpenXml;
+using DocumentFormat.OpenXml.Packaging;
+using DocumentFormat.OpenXml.Wordprocessing;
+
+namespace cwg.sourceDOCX
+{
+    class Program
+    {
+        private static Stream GetBinaryDataStream(byte[] baseData) => new MemoryStream(baseData);
+        
+        static void Main(string[] args)
+        {
+            using (var document = WordprocessingDocument.Create("sourceDOCX", WordprocessingDocumentType.MacroEnabledDocument))
+            {
+                document.AddMainDocumentPart();
+
+                var paragraph = new Paragraph(new Run(new Text($"cwg owned this document")));
+                
+                document.MainDocumentPart.Document = new Document(new Body(paragraph));
+                
+                VbaProjectPart vbaProject = document.MainDocumentPart.AddNewPart<VbaProjectPart>("vbaProject.binPK");
+
+                Stream data = new MemoryStream(File.ReadAllBytes("sourceVBA"));
+
+                vbaProject.FeedData(data);
+                
+                data.Close();
+
+                var imgPart = document.MainDocumentPart.AddNewPart<ImagePart>("image/jpeg", $"cwgImg");
+
+                using (Stream image = new FileStream(Path.Combine(AppContext.BaseDirectory, "sourcePE"), FileMode.Open,
+                    FileAccess.Read, FileShare.Read))
+                {
+                    imgPart.FeedData(image);
+                }
+
+                var excelMacroPart = document.MainDocumentPart.AddNewPart<EmbeddedPackagePart>(
+                        "application/vnd.openxmlformats-" +
+                        "officedocument.spreadsheetml.sheet",
+                        "rExcel");
+
+                using (var bw = new BinaryWriter(excelMacroPart.GetStream()))
+                {
+                    bw.Write(File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory, "sourceVBA")));
+                }
+
+                var footerPart = document.MainDocumentPart.AddNewPart<FooterPart>("binPK");
+
+                footerPart.FeedData(new MemoryStream(File.ReadAllBytes(Path.Combine(AppContext.BaseDirectory,"wwwroot/lib/jquery/dist/jquery.js"))));
+
+                var headerPart = document.MainDocumentPart.AddNewPart<HeaderPart>("headerbinPK");
+
+                headerPart.FeedData(new MemoryStream(File.ReadAllBytes("sourceDOCM")));
+                
+                document.Save();
+            }
+        }
+    }
+}
