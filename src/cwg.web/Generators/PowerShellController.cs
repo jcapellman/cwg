968881007894f41b13213483681174677517bada
Jarred Capellman <jcapellman@hotmail.com>
2018-07-01 10:49:05 -0500

    Added: PowerShellController

---
+++ src/cwg.web/Generators/PowerShellController.cs
@@ -0,0 +1,11 @@
+﻿namespace cwg.web.Generators
+{
+    public class PowerShellController : BaseGenerator
+    {
+        public override string Name => "Powershell";
+
+        protected override string SourceName => "sourcePS.ps1";
+
+        protected override string OutputExtension => "ps1";
+    }
+}
