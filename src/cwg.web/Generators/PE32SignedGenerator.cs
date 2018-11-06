77e8f2f7090a8669ec7b8cafa052e691fea37d8d
Jarred Capellman <jcapellman@hotmail.com>
2018-11-05 21:34:13 -0600

    Added: PE32 Signing Generator

---
+++ src/cwg.web/Generators/PE32SignedGenerator.cs
@@ -0,0 +1,11 @@
+﻿namespace cwg.web.Generators
+{
+    public class PE32SignedGenerator : BaseGenerator
+    {
+        public override string Name => "PE32 SIGNED";
+
+        protected override string SourceName => "sourcePESIGNED.exe";
+
+        protected override string OutputExtension => "exe";
+    }
+}
