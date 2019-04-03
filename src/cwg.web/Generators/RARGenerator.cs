7fc6ad3ca22f606791e94ba59fbefa00e13e3587
Jarred Capellman <jcapellman@hotmail.com>
2019-04-02 21:28:13 -0500

    Change: Refactoring the RARGenerator class to use BaseArchiveGenerator class

--- src/cwg.web/Generators/RARGenerator.cs
+++ src/cwg.web/Generators/RARGenerator.cs
@@ -1,55 +1,13 @@
-﻿using System;
-using System.IO;
-
-using SharpCompress.Common;
-using SharpCompress.Writers;
+﻿using SharpCompress.Common;
 
 namespace cwg.web.Generators
 {
-    public class RARGenerator : BaseGenerator
+    public class RARGenerator : BaseArchiveGenerator
     {
         public override string Name => "RAR";
 
-        protected override string SourceName => "sourcePE.exe";
-
         protected override string OutputExtension => "rar";
 
-        protected override (string sha1, string fileName) Generate()
-        {
-            var fileName = $"{DateTime.Now.Ticks}.{OutputExtension}";
-
-            var originalBytes = File.ReadAllBytes("sourcePE");
-
-            var newBytes = new byte[GetRandomInt()];
-
-            FillArray(newBytes);
-
-            for (var y = 0; y < newBytes.Length; y++)
-            {
-                originalBytes[originalBytes.Length - 1 - y] = newBytes[y];
-            }
-
-            var sha1Sum = ComputeSha1(originalBytes);
-
-            var sourcePEPath = Path.Combine(AppContext.BaseDirectory, $"{sha1Sum}.exe");
-
-            File.WriteAllBytes(sourcePEPath, originalBytes);
-
-            var archiveFileName = Path.Combine(AppContext.BaseDirectory, $"{DateTime.Now.Ticks}.{OutputExtension}");
-
-            using (var zip = File.OpenWrite(archiveFileName))
-            {
-                using (var zipWriter = WriterFactory.Open(zip, ArchiveType.Rar, CompressionType.Rar))
-                {
-                    zipWriter.Write(sha1Sum, sourcePEPath);
-                }
-            }
-
-            var sha1 = ComputeSha1(File.ReadAllBytes(archiveFileName));
-
-            File.Move(archiveFileName, Path.Combine(AppContext.BaseDirectory, $"{sha1}.{OutputExtension}"));
-
-            return (sha1, $"{sha1}.{OutputExtension}");
-        }
+        protected override ArchiveType CurrentArchiveType => ArchiveType.Rar;
     }
 }
