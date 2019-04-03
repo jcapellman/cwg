bd7c254bab56dabd9664df98fd98120f4b6a24d8
Jarred Capellman <jcapellman@hotmail.com>
2019-04-02 21:34:34 -0500

    Added: 7z support

---
+++ src/cwg.web/Generators/SevenZipGenerator.cs
@@ -0,0 +1,13 @@
+﻿using SharpCompress.Common;
+
+namespace cwg.web.Generators
+{
+    public class SevenZipGenerator : BaseArchiveGenerator
+    {
+        public override string Name => "7Z";
+
+        protected override string OutputExtension => "7z";
+
+        protected override ArchiveType CurrentArchiveType => ArchiveType.SevenZip;
+    }
+}
