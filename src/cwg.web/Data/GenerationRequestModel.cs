cd86162518a3e8924e6e10665cb7669c8b0e64b9
Jarred Capellman <jcapellman@hotmail.com>
2020-01-31 22:00:28 -0600

    Added: GenerationRequestModel class

---
+++ src/cwg.web/Data/GenerationRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace cwg.web.Data
+{
+    public class GenerationRequestModel
+    {
+        public string FileType { get; set; }
+
+        public int NumberToGenerate { get; set; }
+
+        public GenerationRequestModel()
+        {
+            NumberToGenerate = 1;
+        }
+    }
+}
