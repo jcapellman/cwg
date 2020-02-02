3b76fa6381ed570845ed1511c3867367590eaf4b
Jarred Capellman <jcapellman@hotmail.com>
2020-02-01 20:59:15 -0600

    Change: Add Generator and Repository for Generators

---
+++ src/cwg.web/Repositories/GeneratoreRepository.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+using cwg.web.Generators;
+
+namespace cwg.web.Repositories
+{
+    public class GeneratoreRepository
+    {
+        private static List<T> GetObjects<T>()
+        {
+            var types = Assembly.GetExecutingAssembly().GetTypes().Where(a => a.BaseType == typeof(T) && !a.IsAbstract);
+
+            return types.Select(b => (T)Activator.CreateInstance(b)).ToList();
+        }
+
+        public IEnumerable<BaseGenerator> GetGenerators()
+        {
+            var baseGenerators = GetObjects<BaseGenerator>();
+
+            baseGenerators.AddRange(GetObjects<BaseArchiveGenerator>());
+
+            return baseGenerators.OrderBy(a => a.Name);
+        }
+    }
+}
--- src/cwg.web/Startup.cs
+++ src/cwg.web/Startup.cs
@@ -1,14 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+
 using cwg.web.Data;
+using cwg.web.Repositories;
 
 namespace cwg.web
 {
@@ -21,16 +18,15 @@ namespace cwg.web
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddRazorPages();
             services.AddServerSideBlazor();
+
+            services.AddSingleton(new GeneratoreRepository());
             services.AddSingleton<GeneratorsService>();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
