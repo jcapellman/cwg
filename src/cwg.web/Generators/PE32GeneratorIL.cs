2dc054e0e55b9a26797b573b92a283454d37b716
Jarred Capellman <jcapellman@hotmail.com>
2020-08-31 20:31:30 -0500

    Added: PE32+ IL

---
+++ src/cwg.web/Generators/PE32GeneratorIL.cs
@@ -0,0 +1,11 @@
+﻿namespace cwg.web.Generators
+{
+    public class PE32PlusILGenerator : BaseGenerator
+    {
+        public override string Name => "PE32+ (IL)";
+
+        protected override string SourceName => "sourcePE+IL.exe";
+
+        protected override string OutputExtension => "exe";
+    }
+}
