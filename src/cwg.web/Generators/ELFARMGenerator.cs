9498a563a8a5d2e7a6133616e935fc22e5136db7
Jarred Capellman <jcapellman@hotmail.com>
2020-10-27 20:15:59 -0500

    Added: ELF ARM Support

---
+++ src/cwg.web/Generators/ELFARMGenerator.cs
@@ -0,0 +1,13 @@
+﻿namespace cwg.web.Generators
+{
+    public class ELFARMGenerator : BaseGenerator
+    {
+        public override string Name => "ELF (ARM)";
+
+        protected override string SourceName => "sourceELFARM";
+
+        protected override string OutputExtension => "elf";
+
+        public override bool Packable => true;
+    }
+}
