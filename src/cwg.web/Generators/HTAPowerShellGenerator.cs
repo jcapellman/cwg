4bc333f321a9ecf5dc56059be8a7b9c2e6b4df8c
Jarred Capellman <jcapellman@hotmail.com>
2020-12-09 19:24:10 -0600

    Added: HTA PowerShell

---
+++ src/cwg.web/Generators/HTAPowerShellGenerator.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+using System.Text;
+
+using cwg.web.Data;
+
+namespace cwg.web.Generators
+{
+    public class HTAPowerShellGenerator : BaseGenerator
+    {
+        public override string Name => "HTA (PowerShell)";
+
+        protected override string SourceName => "sourceHTAPS";
+
+        protected override string OutputExtension => "hta";
+
+        public override bool Packable => false;
+
+        protected override (string sha1, string fileName) Generate(GenerationRequestModel model)
+        {
+            var sourceFile = File.ReadAllText(SourceName);
+
+            var exeString = File.ReadAllText(Path.Combine(AppContext.BaseDirectory, "sourcePS.ps1"));
+
+            sourceFile = sourceFile.Replace("$TIMESTAMP", $"Own3d on {DateTime.Now}");
+
+            sourceFile = sourceFile.Replace("$BASE64PAYLOAD", exeString);
+            
+            var bytes = Encoding.ASCII.GetBytes(sourceFile);
+
+            var sha1Sum = ComputeSha1(bytes);
+
+            File.WriteAllBytes(Path.Combine(AppContext.BaseDirectory, $"{sha1Sum}.{OutputExtension}"), bytes);
+
+            return (sha1Sum, $"{sha1Sum}.{OutputExtension}");
+        }
+    }
+}
