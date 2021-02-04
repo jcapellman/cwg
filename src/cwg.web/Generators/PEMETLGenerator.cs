6c417e88a0019fbd4c921ee00b1c3ef98bdc4b3a
Jarred Capellman <jcapellman@hotmail.com>
2021-02-03 21:13:51 -0600

    Added: METL Generator

---
+++ src/cwg.web/Generators/PEMETLGenerator.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+using cwg.web.Data;
+
+using METL;
+
+namespace cwg.web.Generators
+{
+    public class PEMETLGenerator : BaseGenerator
+    {
+        public override string Name => "PE32 (MET&L)";
+
+        protected override string SourceName => "sourcePE.exe";
+
+        protected override string OutputExtension => ".exe";
+
+        public override bool Packable => false;
+
+        protected override (string sha1, string fileName) Generate(GenerationRequestModel model)
+        {
+            var sourceFile = Path.Combine(AppContext.BaseDirectory, "PE32.cs");
+
+            var malFile = Path.Combine(AppContext.BaseDirectory, SourceName);
+
+            var injectedBytes = METLInjector.InjectMalwareFromFile(sourceFile, malFile);
+
+            var sha = ComputeSha1(injectedBytes);
+
+            var fileName = Path.Combine(AppContext.BaseDirectory, sha + OutputExtension);
+
+            File.WriteAllBytes(fileName, injectedBytes);
+
+            return (sha, fileName);
+        }
+    }
+}
---
+++ src/cwg.web/PE32.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.IO;
+
+namespace METL.InjectorSamples.PE32
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            [MALWARE_EMBED]
+
+            Console.WriteLine("0wn3d by MET&L on [TIMESTAMP]");
+
+            System.IO.File.WriteAllBytes(Path.GetRandomFileName(), Convert.FromBase64String(malSource));
+        }
+    }
+}
