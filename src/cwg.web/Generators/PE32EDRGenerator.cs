c043ffe88ed45d6e21fdb895c907937fb18d997a
Jarred Capellman <jcapellman@hotmail.com>
2022-01-19 21:23:44 -0600

    Added: EDR Generator

---
+++ src/cwg.web/Generators/PE32EDRGenerator.cs
@@ -0,0 +1,17 @@
+﻿namespace cwg.web.Generators
+{
+    public class PE32EDRGenerator : BaseGenerator
+    {
+        public override string Name => "PE32 (EDR)";
+
+        public override bool Active => true;
+
+        protected override string SourceName => "sourcePE32EDR.exe";
+
+        protected override string CleanSourceName => "sourceCleanPE32.exe";
+
+        protected override string OutputExtension => "exe";
+
+        public override bool Packable => true;
+    }
+}
