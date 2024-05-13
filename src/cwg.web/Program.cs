189e834f265fdbee5f50d115646d5ed53bbc8849
Jarred Capellman <jcapellman@hotmail.com>
2024-05-12 21:02:20 -0500

    Change: Updated to use the newer code patterns

--- src/cwg.web/Program.cs
+++ src/cwg.web/Program.cs
@@ -1,4 +1,8 @@
-using Microsoft.AspNetCore.Hosting;
+using cwg.web.Data;
+using cwg.web.Repositories;
+using cwg.web.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 namespace cwg.web
@@ -7,15 +11,54 @@ namespace cwg.web
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebApplication.CreateBuilder(args);
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>()
-                    .UseUrls("http://0.0.0.0:5001");
-                });
+            builder.Services.AddRazorPages();
+            builder.Services.AddServerSideBlazor();
+
+            builder.Services.AddLocalization(options =>
+            {
+                options.ResourcesPath = "Resources";
+            });
+
+            builder.Services.AddScoped<ClipboardService>();
+
+            builder.Services.AddSingleton(new GeneratorRepository());
+            builder.Services.AddSingleton<GeneratorsService>();
+
+            builder.Services.AddControllers();
+
+            builder.Services.AddSwaggerDocument(a =>
+            {
+                a.Description = "Swagger documentation for the cwg services";
+                a.Version = "1.0.2";
+                a.Title = "cwg";
+            });
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseStaticFiles();
+            app.UseAntiforgery();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute("default", "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapBlazorHub();
+                endpoints.MapFallbackToPage("/_Host");
+            });
+
+            app.Run();
+        }
     }
 }
